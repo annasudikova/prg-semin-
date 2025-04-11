1c3f66087504400a5865d749ce18b571aeec8eef
annasudikova <anna.sudikova@gmail.com>
2025-04-11 14:16:14 +0200

    Add files via upload

---
+++ malovani/malovani/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace malovani
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonRed = new System.Windows.Forms.Button();
+            this.buttonBlack = new System.Windows.Forms.Button();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.buttonColor1 = new System.Windows.Forms.Button();
+            this.buttonEraser = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(60, 125);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(679, 289);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_mouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_mouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_mouseUp);
+            // 
+            // buttonRed
+            // 
+            this.buttonRed.BackColor = System.Drawing.Color.Red;
+            this.buttonRed.Location = new System.Drawing.Point(140, 73);
+            this.buttonRed.Name = "buttonRed";
+            this.buttonRed.Size = new System.Drawing.Size(51, 46);
+            this.buttonRed.TabIndex = 1;
+            this.buttonRed.UseVisualStyleBackColor = false;
+            this.buttonRed.Click += new System.EventHandler(this.buttonRed_Click);
+            // 
+            // buttonBlack
+            // 
+            this.buttonBlack.BackColor = System.Drawing.Color.Black;
+            this.buttonBlack.Location = new System.Drawing.Point(73, 73);
+            this.buttonBlack.Name = "buttonBlack";
+            this.buttonBlack.Size = new System.Drawing.Size(51, 46);
+            this.buttonBlack.TabIndex = 2;
+            this.buttonBlack.UseVisualStyleBackColor = false;
+            this.buttonBlack.Click += new System.EventHandler(this.buttonBlack_Click);
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.BackColor = System.Drawing.Color.White;
+            this.buttonNew.Location = new System.Drawing.Point(603, 80);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(135, 38);
+            this.buttonNew.TabIndex = 3;
+            this.buttonNew.Text = "new ";
+            this.buttonNew.UseVisualStyleBackColor = false;
+            this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // buttonColor1
+            // 
+            this.buttonColor1.Location = new System.Drawing.Point(74, 15);
+            this.buttonColor1.Name = "buttonColor1";
+            this.buttonColor1.Size = new System.Drawing.Size(152, 36);
+            this.buttonColor1.TabIndex = 4;
+            this.buttonColor1.Text = "zmena platna";
+            this.buttonColor1.UseVisualStyleBackColor = true;
+            this.buttonColor1.Click += new System.EventHandler(this.buttonColor1_Click);
+            // 
+            // buttonEraser
+            // 
+            this.buttonEraser.Location = new System.Drawing.Point(248, 19);
+            this.buttonEraser.Name = "buttonEraser";
+            this.buttonEraser.Size = new System.Drawing.Size(115, 31);
+            this.buttonEraser.TabIndex = 5;
+            this.buttonEraser.Text = "eraser";
+            this.buttonEraser.UseVisualStyleBackColor = true;
+            this.buttonEraser.Click += new System.EventHandler(this.buttonEraser_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(386, 19);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(172, 69);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.Value);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.buttonEraser);
+            this.Controls.Add(this.buttonColor1);
+            this.Controls.Add(this.buttonNew);
+            this.Controls.Add(this.buttonBlack);
+            this.Controls.Add(this.buttonRed);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonRed;
+        private System.Windows.Forms.Button buttonBlack;
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Button buttonColor1;
+        private System.Windows.Forms.Button buttonEraser;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ malovani/malovani/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace malovani
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        bool mouseDown;
+        Point lastPosition;
+        bool colorRed;
+        bool colorBlack;
+        bool eraser;
+        bool panelBlack;
+        bool myPen;
+        private int trackBarValue = 0; // Proměnná pro uložení hodnoty
+        public Form1()
+        {
+            InitializeComponent();
+            g=panel1.CreateGraphics();
+            panel1.Refresh();
+            InitializeComponent();
+            
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_mouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastPosition = e.Location;
+        }
+
+        private void panel1_mouseMove(object sender, MouseEventArgs e) //barva pera
+        {
+            if (mouseDown == true)
+            {
+                Pen myPen= new Pen(Brushes.Black);
+                myPen.Width= trackBarValue;
+                if (colorRed == true)
+                {
+                    g.DrawLine(Pens.Red, e.Location, lastPosition);
+                    lastPosition = e.Location;
+                }
+                if (colorBlack == true)
+                {
+                    g.DrawLine(Pens.Black, e.Location, lastPosition);
+                    lastPosition = e.Location;
+                }
+                if (eraser == true && panelBlack == true)
+                {
+                    g.DrawLine(Pens.Black, e.Location, lastPosition);
+                    lastPosition = e.Location;
+                }
+
+
+
+
+            }
+        }
+
+        private void panel1_mouseUp(object sender, MouseEventArgs e) 
+        {
+            mouseDown = false;
+        }
+
+        private void buttonBlack_Click(object sender, EventArgs e) //cerne platno
+        {
+            colorBlack = true;
+            colorRed = false;
+            eraser = false;
+        }
+
+        private void buttonRed_Click(object sender, EventArgs e) //cervene pero
+        {
+            colorRed = true;
+            colorBlack= false;
+            eraser = false;
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e) //nove platno
+        {
+            panel1.Refresh();
+        }
+
+        private void buttonColor1_Click(object sender, EventArgs e) //barva pozadi
+        {
+            panel1.BackColor = Color.Black;
+            panelBlack = true;
+        }
+
+        private void buttonEraser_Click(object sender, EventArgs e) //eraser
+        {
+            eraser= true;
+            colorRed = false;
+            colorBlack = false;
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            trackBarValue = trackBar1.Value; // Uložení hodnoty
+            
+        }
+
+        private void Value(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
